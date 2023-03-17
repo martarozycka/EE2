d81f69830e91db536874b62eded3c6ae6e7b0e10
kobe.michiels2 <kobe.michiels2@student.kuleuven.be>
2023-03-17 09:05:55 +0100

    Initialize git

---
+++ User Interface/src/DBTest.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import org.json.*;
+
+public class DBTest {
+
+    public String makeGETRequest(String urlName){
+        BufferedReader rd = null;
+        StringBuilder sb = null;
+        String line = null;
+        try {
+            URL url = new URL(urlName);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            sb = new StringBuilder();
+            while ((line = rd.readLine()) != null)
+            {
+                sb.append(line + '\n');
+            }
+            conn.disconnect();
+            return sb.toString();
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch (ProtocolException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    public Integer[] parseJSON(String jsonString){
+        Integer[] var;
+        var = new Integer[]{0,0,0};
+        try {
+            JSONArray array = new JSONArray(jsonString);
+            for (int i = 0; i < array.length(); i++)
+            {
+                JSONObject curObject = array.getJSONObject(i);
+                var[0] = curObject.getInt("id");
+                var[1] = curObject.getInt("actual_temp");
+                var[2] = curObject.getInt("actual_pres");
+
+
+            }
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+        return var;
+    }
+
+
+    public static void main(String[] args) {
+        DBTest rc = new DBTest();
+        String response = rc.makeGETRequest("https://studev.groept.be/api/a22ib2c01/selectLatestMeasurements" );
+        rc.parseJSON(response);
+    }
+}
---
+++ User Interface/src/ProjectUserInterface.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.util.concurrent.ExecutionException;
+
+public class ProjectUserInterface extends JFrame {
+    private JPanel panel1;
+    private JProgressBar presProgressBar;
+    private JProgressBar tempProgressBar;
+    private JSlider minPresSlider;
+    private JSlider maxPresSlider;
+    private JSlider maxTempSlider;
+    private JSlider minTempSlider;
+    private JLabel minTempValue;
+    private JLabel maxTempValue;
+    private JLabel minPresValue;
+    private JLabel maxPresValue;
+    private JLabel tempValue;
+    private JLabel presValue;
+    private JLabel myTemperature;
+    private JLabel myPressure;
+    private JLabel myTitle;
+    private JLabel minLabelTemp;
+    private JLabel maxLabelTemp;
+    private JLabel minLabelPres;
+    private JLabel maxLabelPres;
+    private JLabel presUnit;
+    private JLabel tempUnit;
+
+    DBTest db = new DBTest();
+
+    public ProjectUserInterface(String title){
+        super(title);
+        // add restriction so that min and max cant be bigger/smaller than actual value
+        // optional: make 3 progress bars: 1 bar form a value lower then min to min, 1 from min to max and 1 from max to a higher value
+        setContentPane(panel1);
+        minTempSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                minTempValue.setText(String.valueOf(source.getValue()));
+                tempProgressBar.setMinimum(Integer.parseInt(minTempValue.getText()));
+                String mint = "/" + minTempValue.getText();
+                String maxt = "/" + maxTempValue.getText();
+                String minp = "/" + minPresValue.getText();
+                String maxp = "/" + maxPresValue.getText();
+                db.makeGETRequest("https://studev.groept.be/api/a22ib2c01/insertMinMax" + mint + maxt + minp + maxp);
+            }
+        });
+        maxTempSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                maxTempValue.setText(String.valueOf(source.getValue()));
+                tempProgressBar.setMaximum(Integer.parseInt(maxTempValue.getText()));
+                String mint = "/" + minTempValue.getText();
+                String maxt = "/" + maxTempValue.getText();
+                String minp = "/" + minPresValue.getText();
+                String maxp = "/" + maxPresValue.getText();
+                db.makeGETRequest("https://studev.groept.be/api/a22ib2c01/insertMinMax" + mint + maxt + minp + maxp);
+            }
+        });
+        minPresSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                minPresValue.setText(String.valueOf(source.getValue()));
+                presProgressBar.setMinimum(Integer.parseInt(minPresValue.getText()));
+                String mint = "/" + minTempValue.getText();
+                String maxt = "/" + maxTempValue.getText();
+                String minp = "/" + minPresValue.getText();
+                String maxp = "/" + maxPresValue.getText();
+                db.makeGETRequest("https://studev.groept.be/api/a22ib2c01/insertMinMax" + mint + maxt + minp + maxp);
+            }
+        });
+        maxPresSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                maxPresValue.setText(String.valueOf(source.getValue()));
+                presProgressBar.setMaximum(Integer.parseInt(maxPresValue.getText()));
+                String mint = "/" + minTempValue.getText();
+                String maxt = "/" + maxTempValue.getText();
+                String minp = "/" + minPresValue.getText();
+                String maxp = "/" + maxPresValue.getText();
+                db.makeGETRequest("https://studev.groept.be/api/a22ib2c01/insertMinMax" + mint + maxt + minp + maxp);
+            }
+        });
+
+
+
+        // dummy data to check interface, actual value will be collected from database (sprint 3)
+        Integer[] measurements_value = db.parseJSON(db.makeGETRequest("https://studev.groept.be/api/a22ib2c01/selectLatestMeasurements"));
+        tempValue.setText(String.valueOf(measurements_value[1]));
+        presValue.setText(String.valueOf(measurements_value[2]));
+        tempProgressBar.setValue(Integer.parseInt(tempValue.getText()));
+        presProgressBar.setValue(Integer.parseInt(presValue.getText()));
+
+
+    }
+
+    public static void main(String[] args) {
+
+        JFrame ui = new ProjectUserInterface(("My title"));
+        ui.setVisible(true);
+        ui.setSize(600,200);
+        ui.setLocationRelativeTo(null);
+    }
+
+
+}
