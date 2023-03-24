081a3588b3187acb577b89aefc84fd7a8dfdc068
kobe.michiels2 <kobe.michiels2@student.kuleuven.be>
2023-03-24 09:28:44 +0100

    Update python code for importing measurements into the DB

--- Temperature and pressure conversion + output .py
+++ Temperature and pressure conversion + output .py
@@ -7,24 +7,31 @@ led = RGBLED(red=2, green=4, blue=3)
 buzzer = Buzzer(17)
 
 while True:
+    borders = requests.get("https://studev.groept.be/api/a22ib2c01/selectMinMax")
+    jsonresponse = borders.json()[0]
+    minTemp = jsonresponse["min_temp"]
+    maxTemp = jsonresponse["max_temp"]
+    minPres = jsonresponse["min_pres"]
+    maxPres = jsonresponse["max_pres"]
     temperature = ((MCP3008(channel=0).value * 3.3 - 0.73) * 45.2 - 33)
     print(temperature)
     pressure = MCP3008(channel=1).value
     if pressure < 0.0005:
         pressure = 0
     print(pressure)
-    if pressure <= 0.4:
+    if pressure <= maxPres:
         buzzer.off()
     else:
         buzzer.on()
-    if temperature > 30:
+    if temperature > maxTemp:
         led.color = (1, 0, 0)
-    elif 22 < temperature < 30:
+    elif minTemp < temperature < maxTemp:
         led.color = (0,1 ,0)
     else:
         led.color = (0, 0, 1)
     sleep(1)
     requests.get("https://studev.groept.be/api/a22ib2c01/InsertMeasurementValue/"+str(temperature)+"/" + str(pressure))
+
 
 
 
